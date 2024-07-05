8fa47629a1728e9dcba9a98d510f7820c36b4ccc
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-05 15:55:20 +0530

    Removing all the files in the repository

--- Scripts/preloader.js
+++
@@ -1,7 +0,0 @@
-    // Example of hiding the preloader after the page content is loaded
-    window.addEventListener('load', function () {
-        var preloaderOverlay = document.querySelector('.preloader-overlay');
-        if (preloaderOverlay) {
-            preloaderOverlay.style.display = 'none';
-        }
-    });
